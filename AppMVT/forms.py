0c1674df7a229ae1889b16a9b23762a17605c370
marjoriesoares <marjoriemms@hotmail.com>
2022-08-27 17:27:55 -0300

    Adicionei o about me

--- AppMVT/forms.py
+++ AppMVT/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.forms import TextInput, EmailInput
 
 class ContactForm(forms.Form):
-    full_name=forms.CharField(max_length=50)
-    email=forms.EmailField()
-    phone=forms.IntegerField()
-    message=forms.CharField(widget=forms.Textarea)
+    Nombre=forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Ingrese su nombre completo', 'style': 'width: 246px;'}))
+    Email=forms.EmailField(widget=forms.EmailInput(attrs={'placeholder' :'Ingrese su correo electrónico', 'style': 'width: 246px;'}))
+    Teléfono=forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder' :'Ingrese su teléfono', 'style': 'width: 246px;'}))
+    Mensaje=forms.CharField(max_length = 300,widget=forms.Textarea(attrs={'placeholder': 'Ingrese un mensaje de hasta 300 caracteres', 'style': 'width: 246px;'}))
