6baef64bfe3ccdc76bcc812fecf6649edec1d64a
marjoriesoares <marjoriemms@hotmail.com>
2022-09-01 19:42:03 -0300

    Cleaned not used files in main project

--- MVT/urls.py
+++ MVT/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from MVT.views import inicio, appMVT, bootstrap
+from MVT.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicio),
     path('AppMVT/', include('AppMVT.urls')),
-    path('iniciobackup/', bootstrap)
 ]
--- MVT/views.py
+++ MVT/views.py
@@ -1,13 +1,5 @@
-from http.client import HTTPResponse
 from django.http import HttpResponse
-from django.template import Template, Context, loader
 from django.shortcuts import render, redirect
 
 def inicio(request):
     return redirect('/AppMVT')
-
-def appMVT(request):
-    return render(request, 'inicio.html')
-
-def bootstrap(request):
-    return render(request, 'MVT/iniciobackup.html')
